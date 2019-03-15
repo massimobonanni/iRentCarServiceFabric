297e463375de65b0a6e3cb71e5cd6577b6c20905
Massimo Bonanni <mabonann@microsoft.com>
2019-03-15 13:57:19 +0100

    Update Service Fabric packages

--- Actors/iRentCar.VehicleActor.Interfaces/IVehicleActor.cs
+++ Actors/iRentCar.VehicleActor.Interfaces/IVehicleActor.cs
@@ -7,7 +7,7 @@ using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
 using Microsoft.ServiceFabric.Services.Remoting;
 
-[assembly: FabricTransportActorRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+//[assembly: FabricTransportActorRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
 namespace iRentCar.VehicleActor.Interfaces
 {
 
